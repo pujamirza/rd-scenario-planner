4f0e4be4a1f55ea36a0ee21983f48889787f5128
pujamirza <pmirza@natera.com>
2025-06-30 21:03:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("R&D Resource Scenario Planner")
 uploaded_file = st.file_uploader("Upload your allocation data (CSV)", type="csv")
 
 if uploaded_file:
-    df = pd.read_csv(uploaded_file)
+df = pd.read_csv(uploaded_file, encoding='utf-8', sep=",", engine="python", on_bad_lines='skip')
     st.success("File loaded!")
 
     # Show options
